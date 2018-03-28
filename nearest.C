19042878b79f279026814f4f7c0b611704efa237
harithanagaraj <36696201+harithanagaraj@users.noreply.github.com>
2018-03-28 16:38:33 +0530

    prgm

---
+++ nearest.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+    
+    int a, b, c;
+    printf("Enter a,b,c: ");
+    scanf("%d %d %d", &a, &b, &c);
+    
+    int(i=0;i++;I>4);
+    
+    if (a > b && a > c) {
+        printf("a is Greater than b and c");
+    }
+    else if (b > a && b > c) {
+        printf("b is Greater than a and c");
+    }
+    else if (c > a && c > b) {
+        printf("c is Greater than a and b");
+    }
+    else {
+        printf("all are equal or any two values are equal");
+    } 
+    return 0;
+}
